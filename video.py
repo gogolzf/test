3dfa3d68be2e05fd9a488378fda613785cc5a245
[v_zhiflin] <v_zhiflin@tencent.com>
2019-06-17 17:05:01 +0800

    第二次提交

--- video.py
+++ video.py
@@ -4,6 +4,8 @@ Author: 林志帆
 '''
 import cv2
 vc = cv2.VideoCapture("G:/MyStudy/Python/1.mp4")
+vc = cv2.VideoCapture("G:/MyStudy/Python/1.mp4")
+vc = cv2.VideoCapture("G:/MyStudy/Python/1.mp4")
 i = 0
 rate = 50 #保存频率
 rval = vc.isOpened()
